17dee0465b7d74bc03c5f50f4d0ecd72b46eeff9
Khaleel Younis <Khaleelyounis.dev@gmail.com>
2020-07-21 20:43:44 -0700

    editBlogPost

--- blog/src/Screens/EditScreen.js
+++ blog/src/Screens/EditScreen.js
@@ -4,19 +4,21 @@ import { Context } from '../context/blogContext';
 import BlogPostForm from '../components/BlogPostForm';
 
 const EditScreen = ({ navigation }) => {
-  const { state } = useContext(Context);
-  const blogPost = state.find(
-    (blogPost) => (blogPost.id = navigation.getParam('id')),
-  );
+  const id = navigation.getParam('id');
+  const { state, editBlogPost } = useContext(Context);
+  const blogPost = state.find((blogPost) => (blogPost.id = id));
   const [title, setTitle] = useState(blogPost.title);
   const [content, setContent] = useState(blogPost.content);
   const initialValues = { title: blogPost.title, content: blogPost.content };
   const onSubmit = (title, content) => {
-    console.log(title, content);
+    editBlogPost(id, title, content, () => navigation.pop());
   };
 
   return (
-    <BlogPostForm onSubmit={() => onSubmit()} initialValues={initialValues} />
+    <BlogPostForm
+      onSubmit={(title, content) => onSubmit(title, content)}
+      initialValues={initialValues}
+    />
   );
 };
 
